d947357b237a2e420e401e0c46269a5aed983eea
Someshwer Bandapally <bsomeshwer89@gmail.com>
2018-08-19 14:12:07 +0530

    info() method and info route is added

--- src/Lib/Firewall.php
+++ src/Lib/Firewall.php
@@ -50,6 +50,28 @@ class Firewall
     }
 
     /**
+     * This package provides some useful information about the package.
+     * @return array
+     */
+    public function info()
+    {
+        return [
+            'package_name' => 'Laravel - Firewall',
+            'description' => 'Laravel Firewall package detects unknown ip addresses based on 
+            blacklist and whitelist ip addresses. Whitelist and Blacklist are two configuration options 
+            any one of them you can set to TRUE based on your requirement. For example if you set blacklist
+            to TRUE and have added some ip addresses to blacklist then in that case any request to the 
+            application will be blocked by the firewall from those ip addresses that listed in blacklist.
+            If you have added them to whitelist only the request from the whitelisted ips can be accepted 
+            and remaining all requests will be blocked by the firewall. If you set both black and whitelist 
+            to TRUE then in that case the preference will be given to blacklist',
+            'latest_release' => '1.2.4',
+            'stable_version' => '1.2.4',
+            'author' => 'Someshwer Bandapally<bsomeshwer89@gmail.com>'
+        ];
+    }
+
+    /**
      * Returns all whitelisted ip addresses
      * @return array
      */
--- src/Routes/routes.php
+++ src/Routes/routes.php
@@ -1,13 +1,27 @@
 <?php
 
-Route::get('sayHello', function () {
-    // dd('Hello World!!');
+
+/**
+ * @author Someshwer Bandapally
+ * Date: 19-08-2018
+ *
+ * This route provides some useful information about the package.
+ */
+Route::get('firewall/info', function () {
+
+    return [
+        'package_name' => 'Laravel - Firewall',
+        'description' => 'Laravel Firewall package detects unknown ip addresses based on 
+            blacklist and whitelist ip addresses. Whitelist and Blacklist are two configuration options 
+            any one of them you can set to TRUE based on your requirement. For example if you set blacklist
+            to TRUE and have added some ip addresses to blacklist then in that case any request to the 
+            application will be blocked by the firewall from those ip addresses that listed in blacklist.
+            If you have added them to whitelist only the request from the whitelisted ips can be accepted 
+            and remaining all requests will be blocked by the firewall. If you set both black and whitelist 
+            to TRUE then in that case the preference will be given to blacklist',
+        'latest_release' => '1.2.4',
+        'stable_version' => '1.2.4',
+        'author' => 'Someshwer Bandapally<bsomeshwer89@gmail.com>'
+    ];
+
 });
-
-
-Route::get('testModel', function () {
-
-    // $fireIp->create(['path'=>'to']);
-
-});
-
